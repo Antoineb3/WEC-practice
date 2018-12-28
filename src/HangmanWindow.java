831adbdd2371707f37ac2f3557966898904f6209
Dylan-Gordon <dylan.gordon.10@gmail.com>
2018-12-28 13:42:16 -0700

    added images of hangman

--- src/HangmanWindow.java
+++ src/HangmanWindow.java
@@ -7,5 +7,14 @@ public class HangmanWindow extends JFrame{
 	 */
 	private static final long serialVersionUID = -2162141288814846399L;
 	
+	// Hangman diagram
+	
+	// wrong guesses box
+	
+	// new guess box (small)
+	
+	// button to submit guess
+	
+	// Layout of the word to guess
 
 }
